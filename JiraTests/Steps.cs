ee0eaf5b3694f37065d8cd1f92a52b3cf7473b42
Georgii Kalnytskyi <gkalnytskyi@gmail.com>
2014-12-03 20:15:02 +0200

    All other infrastructure classes, steps and Tests

---
+++ JiraTests/Steps.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using FluentAssertions;
+
+namespace JiraTests
+{
+    public class Steps
+    {
+        private readonly AutomationFramework _Af;
+
+        public Steps(AutomationFramework af)
+        {
+            _Af = af;
+        }
+
+        public virtual void GoToProjectSummaryPage()
+        {
+            _Af.GoToUrl("browse/TST/");
+        }
+
+        public virtual void CreateNewBugIssue(string id)
+        {
+            using (var page = new BasePage(_Af))
+            {
+                var testMessage = "Test bug issue " + id;
+                page.PressCreateIssueButton();
+                var createIssue = new CreateIssueWidget("create-issue-dialog", _Af);
+                createIssue.SelectIssueType("Bug");
+                createIssue.EnterSummary(testMessage);
+                createIssue.EnterDescription(testMessage);
+                createIssue.PressSubmitButton();
+            }
+        }
+
+        public virtual void GoToProjectsIssuesSummary()
+        {
+            _Af.GoToUrl("browse/TST/?selectedTab=com.atlassian.jira.jira-projects-plugin:issues-panel");
+        }
+
+        public virtual void GoToIssuesReportedByMe()
+        {
+            var issuesSummary = new IssuesSummaryWidget("project-tab", _Af);
+            issuesSummary.PressReportedByMe();
+        }
+
+        public virtual void GoToIssuePage(string issueIdentifier)
+        {
+            using (var issueReportPage = new IssueSearchResultPage(_Af))
+            {
+                issueReportPage.GoToIssue(issueIdentifier);
+            }
+        }
+
+        public virtual void UpdateIssue()
+        {
+            using (var issuePage = new IssuePage(_Af))
+            {
+                issuePage.PressEditButton();
+                var editWidget = new EditIssueWidget("edit-issue-dialog",_Af);
+
+                var oldDescription = editWidget.Description;
+                editWidget.EnterDescription(Keys.Enter + "Issue updated.");
+                editWidget.PressUpdateButton();
+            }
+        }
+
+        public virtual void VerifyIssueCreated(string issueIdentifier)
+        {
+            using (var issueReportPage = new IssueSearchResultPage(_Af))
+            {
+                var issues = issueReportPage.IssuesReport.Issues;
+                var issue = issues.FirstOrDefault(x => x.Summary.Contains(issueIdentifier));
+                issue.Should().NotBeNull();
+                issue.Status.Should().Be("OPEN");
+                issue.IssueType.Should().Be("Bug");
+            }
+        }
+
+        public virtual void VerifyIssueUpdated()
+        {
+            using (var issuePage = new IssuePage(_Af))
+            {
+                var description = issuePage.Description;
+                description.Should().EndWith("Issue updated.");
+            }
+        }
+    }
+}
---
+++ JiraTests/Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace JiraTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        protected static AutomationFramework _Af;
+        protected Steps _Steps;
+        
+        [TestFixtureSetUp]
+        public static void FixtureSetUp()
+        {
+            try
+            {
+                _Af = new AutomationFramework("https://jira.atlassian.com/");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [TestFixtureTearDown]
+        public static void FixtureTeardown()
+        {
+            _Af.Dispose();
+        }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _Steps = new Steps(_Af);
+
+            Setup();
+        }
+
+        protected virtual void Setup() { }
+
+        [Test]
+        public void User_Can_Create_New_Issue()
+        {
+            // Arrange
+            var issueIdentifier = Guid.NewGuid().ToString();
+
+            // Act
+            _Steps.GoToProjectSummaryPage();
+            _Steps.CreateNewBugIssue(issueIdentifier);
+            _Steps.GoToProjectsIssuesSummary();
+            _Steps.GoToIssuesReportedByMe();
+
+            // Assert
+            _Steps.VerifyIssueCreated(issueIdentifier);
+        }
+
+        [Test]
+        public void User_Can_Edit_Issue()
+        {
+            // Arrange
+            // var issueIdentifier = Guid.NewGuid().ToString();
+            var issueIdentifier = "38d3d4da-d497-4192-bb7a-c98f072310b7";
+            _Steps.GoToProjectsIssuesSummary();
+            //_Steps.CreateNewBugIssue(issueIdentifier);
+            // Act
+            _Steps.GoToIssuesReportedByMe();
+            _Steps.GoToIssuePage(issueIdentifier);
+            _Steps.UpdateIssue();
+
+            // Assert
+            _Steps.VerifyIssueUpdated();
+        }
+    }
+}
