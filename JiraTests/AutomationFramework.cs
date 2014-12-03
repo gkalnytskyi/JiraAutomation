fcb9a548f23b1623f32e05036d114102444bafc0
Georgii Kalnytskyi <gkalnytskyi@gmail.com>
2014-12-03 20:01:32 +0200

    Base infrastructure for Selenium

---
+++ JiraTests/AutomationFramework.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace JiraTests
+{
+    public class AutomationFramework : IDisposable
+    {
+        private Uri _BaseUrl;
+        public const int Timeout = 10;
+
+        public IWebDriver Driver { get; private set; }
+
+        public AutomationFramework(string baseUri)
+        {
+            _BaseUrl = new Uri(baseUri);
+            Init();
+        }
+
+        public void GoToUrl(string relativeUrl)
+        {
+            var uri = new Uri(_BaseUrl, relativeUrl);
+            Driver.Navigate().GoToUrl(uri);
+        }
+
+        private void Init()
+        {
+            StopAllDrivers();
+
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--user-data-dir=c:/Users/Georgii/AppData/Local/Google/Chrome/User Data");
+            Driver = new ChromeDriver(options);
+            Driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(Timeout));
+        }
+        
+        private void StopAllDrivers()
+        {
+            foreach (var process in Process.GetProcessesByName("chromedriver"))
+            {
+                process.Kill();
+            }
+        }
+
+        #region Dispose Pattern
+        public void Dispose()
+        {
+            if (Driver != null)
+            {
+                Driver.Close();
+                Driver.Quit();
+                Driver.Dispose();
+                Driver = null;
+            }
+            GC.SuppressFinalize(this);
+        }
+
+        ~AutomationFramework()
+        {
+            Dispose();
+        }
+        #endregion
+    }
+}
